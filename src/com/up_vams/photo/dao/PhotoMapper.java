8894807246a2b5430e9049425bb6fa568b070925
wzw <wzw33@outlook.com>
2017-04-05 18:24:12 +0800

    Photo,School

---
+++ src/com/up_vams/photo/dao/PhotoMapper.java
@@ -0,0 +1,13 @@
+package com.up_vams.photo.dao;
+
+
+import com.up_vams.core.dao.BaseMapper;
+import com.up_vams.photo.entity.Photo;
+
+public interface PhotoMapper extends BaseMapper<Photo>{
+
+
+
+
+
+}
---
+++ src/com/up_vams/photo/entity/Photo.java
@@ -0,0 +1,98 @@
+package com.up_vams.photo.entity;
+
+import java.io.Serializable;
+
+public class Photo implements Serializable{
+
+    private static final long serialVersionUID = 5782293761762720735L;
+
+    private String photoId;
+
+    private String name;
+
+    private String title;
+
+    private Integer latitude;
+
+    private Integer longitude;
+
+    private Integer viewNum;
+
+    private Integer likeNum;
+
+    private String creatorId;
+
+    private String introduction;
+
+    public String getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(String photoId) {
+        this.photoId = photoId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Integer latitude) {
+        this.latitude = latitude;
+    }
+
+    public Integer getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Integer longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getViewNum() {
+        return viewNum;
+    }
+
+    public void setViewNum(Integer viewNum) {
+        this.viewNum = viewNum;
+    }
+
+    public Integer getLikeNum() {
+        return likeNum;
+    }
+
+    public void setLikeNum(Integer likeNum) {
+        this.likeNum = likeNum;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+}
