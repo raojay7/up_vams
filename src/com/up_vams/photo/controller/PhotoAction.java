6e0eb3382a287e95a247e1c0a17d63a2ae0f5691
raojay7 <879645920@qq.com>
2017-04-05 18:25:03 +0800

    两个控制层

---
+++ src/com/up_vams/photo/controller/PhotoAction.java
@@ -0,0 +1,8 @@
+package com.up_vams.photo.controller;
+
+/**
+ * Created by 隽 on 2017/4/5.
+ */
+public class PhotoAction
+{
+}
---
+++ src/com/up_vams/school/controller/SchoolAction.java
@@ -0,0 +1,9 @@
+package com.up_vams.school.controller;
+
+/**
+ * Created by 隽 on 2017/4/5.
+ */
+
+public class SchoolAction
+{
+}
