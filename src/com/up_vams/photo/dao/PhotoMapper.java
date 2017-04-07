b09b0feab6923ec4c35361fe1d030e7be3aaa04f
wzw <wzw33@outlook.com>
2017-04-07 21:38:18 +0800

    PhotoMapper修改

--- src/com/up_vams/photo/dao/PhotoMapper.java
+++ src/com/up_vams/photo/dao/PhotoMapper.java
@@ -2,7 +2,11 @@ package com.up_vams.photo.dao;
 
 
 import com.up_vams.core.dao.BaseMapper;
+import com.up_vams.core.entity.Page;
 import com.up_vams.photo.entity.Photo;
+import com.up_vams.schoolPhoto.entity.SchoolPhoto;
+
+import java.util.List;
 
 public interface PhotoMapper extends BaseMapper<Photo>{
 
@@ -14,6 +18,7 @@ public interface PhotoMapper extends BaseMapper<Photo>{
      */
     Photo selectSchoolMore(String photoId);
 
+    List<Photo> selectPhotos(Page<SchoolPhoto> page);
 
 
 
