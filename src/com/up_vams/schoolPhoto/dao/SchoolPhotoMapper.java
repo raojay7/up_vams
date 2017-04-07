7e5b387398e20d819e0b4cfa429fe6e30a59df0f
wzw <wzw33@outlook.com>
2017-04-07 19:18:58 +0800

    新增方法首页图片查询

--- src/com/up_vams/schoolPhoto/dao/SchoolPhotoMapper.java
+++ src/com/up_vams/schoolPhoto/dao/SchoolPhotoMapper.java
@@ -3,7 +3,15 @@ package com.up_vams.schoolPhoto.dao;
 import com.up_vams.core.dao.BaseMapper;
 import com.up_vams.schoolPhoto.entity.SchoolPhoto;
 
+import java.util.List;
+
 public interface SchoolPhotoMapper extends BaseMapper<SchoolPhoto>{
 
+    /**
+     * 通过学校ID查询照片ID列表
+     * @param schoolId 学校ID
+     * @return 实体列表
+     */
+    List<SchoolPhoto> selectPhotos(String schoolId);
 
 }
