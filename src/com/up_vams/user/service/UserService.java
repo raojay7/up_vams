a3ca1bff0bc517c71a95861db78e09ce6d9e22a2
raojay7 <879645920@qq.com>
2017-04-22 17:29:05 +0800

    定义了需要的接口

--- src/com/up_vams/user/service/UserService.java
+++ src/com/up_vams/user/service/UserService.java
@@ -1,11 +1,31 @@
 package com.up_vams.user.service;
 
+import com.up_vams.core.entity.Page;
 import com.up_vams.core.service.BaseService;
+import com.up_vams.photo.entity.Photo;
 import com.up_vams.user.entity.User;
+import com.up_vams.userPhoto.entity.UserPhoto;
+
+import java.util.List;
 
 /**
  * Created by 隽 on 2017/3/29.
  */
 public interface UserService extends BaseService<User>
 {
+    /**
+     * 根据已经封装好的page查询userphoto
+     * 真正有用的数据：
+     * pageNum, 8, totalRecord，userid
+     * @param page
+     * @return 8条userphoto数据
+     */
+    List<UserPhoto> findUserPhotoByPage(Page<UserPhoto> page);
+
+    /**
+     * 根据8条userphoto数据，来查询对应的photo
+     * @param list 前面分页查询到的list
+     * @return 得到9条照片数据
+     */
+    List<Photo> selectPhotosByUser(List<UserPhoto> list);
 }
