049f73b1800730cf479787220cc792066387035f
wzw <wzw33@outlook.com>
2017-04-22 11:52:30 +0800

    photo_ossname

---
+++ src/com/up_vams/photoDiscussion/dao/PhotoDiscussionMapper.java
@@ -0,0 +1,8 @@
+package com.up_vams.photoDiscussion.dao;
+
+import com.up_vams.core.dao.BaseMapper;
+import com.up_vams.photoDiscussion.entity.PhotoDiscussion;
+
+public interface PhotoDiscussionMapper extends BaseMapper<PhotoDiscussion>{
+
+}
---
+++ src/com/up_vams/photoDiscussion/entity/PhotoDiscussion.java
@@ -0,0 +1,23 @@
+package com.up_vams.photoDiscussion.entity;
+
+public class PhotoDiscussion{
+    private String discussionId;
+
+    private String photoId;
+
+    public String getDiscussionId() {
+        return discussionId;
+    }
+
+    public void setDiscussionId(String discussionId) {
+        this.discussionId = discussionId;
+    }
+
+    public String getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(String photoId) {
+        this.photoId = photoId;
+    }
+}
